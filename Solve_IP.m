df5f13063083e03a1adc226da8a10a9f1f7c9900
Ivan Zhou <ivan.zhouyq@gmail.com>
2017-04-15 13:31:45 -0400

    Fix bug in region filter

--- Solve_IP.m
+++ Solve_IP.m
@@ -1,4 +1,4 @@
-function [x] = Solve_IP(max_hours_per_week,time_slot_available,region_avilable,avg_revenue_trip,p_max,p_min)
+function [x,obj_ip,time_ip] = Solve_IP(max_hours_per_week,time_slot_available,region_available,avg_revenue_trip,p_max,p_min)
 %% This function solve the IP Model of Uber Driver Scheduling Problem
 % max_hours_per_week: Maximum number of hours the driver can contribute per
 % week
@@ -38,7 +38,8 @@ weekday_choices = 1:7;
 time_choices = [0 1 7 8 16 17 18 19 20 21 22 23];
 timeslot_choices = [0 1 2 3 4 5]; % Timeslots that cooresponds to the above time_choices
 n_weekdays_choices = size(weekday_choices,2);
-n_timeslot_choices = size(weekday_choices,2);
+% n_timeslot_choices = size(weekday_choices,2);
+n_timeslot_choices = size(timeslot_choices,2);
 
 % Initialize indices of element to be removed: all are 0 in the beginning
 indices_to_be_rm = zeros(size(Weekday));
@@ -60,17 +61,23 @@ for d = weekday_choices
 end
 
 % Mark unavailable reigons
-% We need to add 1 to the index of region
-if sum(region_avilable)<5
-    indices_to_be_rm(Region == find(region_avilable==0)-1) = 1;
+if sum(region_available)<5
+    % find(region_avilable==0)-1 because the index in region starts with 0
+    unavailable_region = find(region_available==0)-1;
+    % If the regions in the unavailable_region shows in Region, it will be
+    % marked with 1
+    unavailable_region_ind = ismember(Region, transpose(unavailable_region));
+    indices_to_be_rm(unavailable_region_ind) = 1;
 end
 
 % Removed all marked instances
 % We need to backwards
 if sum(indices_to_be_rm) >0
     ind_to_be_removed = find(indices_to_be_rm == 1);
+    %ind_to_be_removed
     for i = sum(indices_to_be_rm):-1:1
         ind = ind_to_be_removed(i);
+        %size(Weekday)
         Weekday(ind) = [];
         Timeslot(ind) = [];
         Min_Trips(ind) = [];
@@ -110,7 +117,10 @@ A = [A1;A2];
 b = [b1;b2];
 lb = zeros(n_x,1); 
 ub = ones(n_x,1);
+tic;
 [x,fval] = intlinprog(-f,intcon,A,b,[],[],lb,ub);
+time_ip = toc;
+obj_ip = -1*fval;
 
 %% Present the Solution
 select_list = find(x==1);
@@ -128,13 +138,14 @@ for i = 1:sum(x)
     
     weekday_name = day(day_number_benchmark+select_day,'name');
     timeslot = timeslots(select_time +1);
-    string_output = ['Recommendation ' num2str(i) ': Region ' num2str(select_region) ', ' timeslot ', ' weekday_name];
+    %string_output = ['Recommendation ' num2str(i) ': Region ' num2str(select_region) ', ' timeslot ', ' weekday_name];
     
     %p_new = P_new_customer(select_list(i));
     %min_trip = Min_Trips(select_list(i));
     %string_output = ['Recommendation ' num2str(i) ': Region ' num2str(select_region) ', ' timeslot ', ' weekday_name ', ' p_new  ', ' min_trip];
-    disp(string_output)
+    
+    %disp(string_output)
 end
 
-string_output = ['Total Revenue Earned: ' num2str(-1*fval)];
-disp(string_output)
+%string_output = ['Total Revenue Earned: ' num2str(obj_ip)];
+%disp(string_output)
