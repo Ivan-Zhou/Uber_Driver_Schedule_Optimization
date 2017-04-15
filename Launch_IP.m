4ea6083bf7e610f3840f899570b350e05ecff191
Ivan Zhou <ivan.zhouyq@gmail.com>
2017-04-15 13:32:24 -0400

    Modify region available variable

--- Launch_IP.m
+++ Launch_IP.m
@@ -7,12 +7,11 @@ time_slot_available = ones(7,24); % Represent the timeslot when the driver is av
 %time_slot_available(6:7,:) = 0;
 %time_slot_available(:,1:18) = 0;
 
-region_avilable = ones(5,1); % Indicate which of the 5 regions the driver is avilable to go
-
+region_available = ones(5,1); % Indicate which of the 5 regions the driver is avilable to go
 avg_revenue_trip = 12; % Average Revenue the Driver get per trip
 p_max = 1; % Maximum Probability that the Driver can get customers - best case scenario
 p_min = 0.5; % Minimum Probability that the Driver can get customers - worse case scenario
 
 
 %% Launch the IP Solver
-[x] = Solve_IP(max_hours_per_week,time_slot_available,region_avilable,avg_revenue_trip,p_max,p_min);
+[x,obj_ip,time_ip] = Solve_IP(max_hours_per_week,time_slot_available,region_available,avg_revenue_trip,p_max,p_min);
